f1ea7cdb64f0baa12d884d072c1ab666ae943388
rajaNayak123 <nayakraja151@gmail.com>
2025-02-12 15:38:16 +0530

    handel the file uploading

---
+++ app/api/imageKit-auth/route.ts
@@ -0,0 +1,20 @@
+import ImageKit from "imagekit"
+import { NextResponse } from "next/server";
+
+const imagekit = new ImageKit({
+  publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY!,
+  privateKey: process.env.PRIVATE_KEY!,
+  urlEndpoint: process.env.NEXT_PUBLIC_URL_ENDPOINT!,
+});
+
+export async function GET() {
+    try {
+        const authenticationParameter = imagekit.getAuthenticationParameters()
+        return NextResponse.json(authenticationParameter);
+    } catch (error) {
+        return NextResponse.json(
+            {error},
+            {status:500}
+        )
+    }
+}
---
+++ app/components/FileUpload.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { IKUpload } from "imagekitio-next";
+import { IKUploadResponse } from "imagekitio-next/dist/types/components/IKUpload/props";
+import { useState } from "react";
+
+export default function FileUpload({onSuccess,}: {onSuccess: (res: IKUploadResponse) => void;}) {
+  const [uploading, setUploading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const onError = (err: { message: string }) => {
+    setError(err.message);
+    setUploading(false);
+  };
+
+  const handelSuccess = (response: IKUploadResponse) => {
+    setUploading(false);
+    setError(null);
+    onSuccess(response);
+  };
+
+  const handleStartUpload = () =>{
+    setUploading(true);
+    setError(null);
+  }
+
+  return (
+    <div className="space-y-2">
+        <h2>File upload</h2>
+        <IKUpload
+           fileName="product-image.png" 
+           onError={onError} 
+           onSuccess={handelSuccess} 
+           onUploadStart={handleStartUpload}
+           validateFile={(file:File)=>{
+            const fileType = ["image/png", "image/jpeg", "image/webp"]
+            if(fileType.includes(file.type)){
+                setError("Invalid file type")
+            }
+
+            if(file.size > 10 * 1024 * 1024){
+                setError("File size must less than 10 MB")
+            }
+            return true;
+           }}
+        />
+
+        {uploading && (<p className="text-sm font-medium text-zinc-500">Uploading ...</p>)}
+        {error && (<p className="text-sm text-red-600">{error}</p>)}
+    </div>
+)}
