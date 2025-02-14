eaa5a16e1620d11ddfd9f7f97e1bf4615507ce94
rajaNayak123 <nayakraja151@gmail.com>
2025-02-14 17:14:13 +0530

    Complete paymentgetway feature

--- app/api/webhook/rezorpay/route.ts
+++ app/api/webhook/rezorpay/route.ts
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 import crypto from "crypto"
-import { json } from "stream/consumers";
 import { connectToDatabase } from "@/lib/db";
 import Order from "@/models/Order";
 import nodemailer from 'nodemailer';
@@ -36,10 +35,32 @@ export async function POST(req:NextRequest){
             ])
 
             if(order){
-                
+               const transporter = nodemailer.createTransport({
+                    service:"sandbox.smtp.mailtrap.io",
+                    port:2525,
+                    auth:{
+                        user:process.env.NODEMAILER_USER,
+                        pass:process.env.NODEMAILER_PASS
+                    }
+               }) 
+               await transporter.sendMail({
+                    from:"nayak@gmail.com",
+                    to:order.userId.email,
+                    subject:"Order completed",
+                    text:`your order${order.productId.name} has been successfully placed`
+               })
             }
         }
+
+        return NextResponse.json(
+            {message:"success"},
+            {status:200}
+        )
     } catch (error) {
-        
+        console.log(error);
+        return NextResponse.json(
+            {error:"error while payment"},
+            {status:500}
+        )
     }
 }
